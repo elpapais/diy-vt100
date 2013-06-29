83c2ad93a2878fadd8ec5385fd055ff854bd07c2
Kuldeep Singh Dhaka <kuldeepdhaka9@gmail.com>
2013-06-29 10:27:56 +0530

    makefile variables rename

--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ fi
 
 export PROGNAME=diy-vt100
 export SRC_DIR=$PWD
-export HW_DIR=$PWD/port/$1
+export PORT_DIR=$PWD/port/$1
 export BUILD_DIR=$PWD/build
 export INCLUDE_DIR=$SRC_DIR/include
 
-cd $HW_DIR
+cd $PORT_DIR
 make $2
